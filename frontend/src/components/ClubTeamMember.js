7923010791d99576547e856cefa4ee4e35ae2756
shanmukh-sai2003 <104721888+shanmukh-sai2003@users.noreply.github.com>
2023-10-17 23:30:14 +0530

    add delete team member feature

--- frontend/src/components/ClubTeamMember.js
+++ frontend/src/components/ClubTeamMember.js
@@ -1,10 +1,20 @@
+import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import AdminContext from "../utils/context/AdminContext";
+import DeleteTeamMember from "./club-admin/DeleteTeamMember";
+
 function ClubTeamMember(props) {
-    const {name, role} = props;
+    const {name, role, id} = props;
+    const location = useLocation();
+    const clubId = location.pathname.split('/')[2];
+    const {admin} = useContext(AdminContext);
+
     return (
         <div className="flex flex-col items-center mb-8 mx-4 w-3/12">
             <img className="w-[150px] rounded-full shadow-xl" src={require("../images/gdsc-logo.png")}></img>
             <h3 className="font-bold text-2xl">{name}</h3>
             <h5 className="text-xl">{role}</h5>
+            {admin.clubId === clubId && <DeleteTeamMember roleId={id} clubId={clubId}/>}
         </div>
     );
 }
--- frontend/src/components/club-admin/AddEvent.js
+++ frontend/src/components/club-admin/AddEvent.js
@@ -24,7 +24,6 @@ function AddEvent() {
         });
 
         const data = await respone.json();
-        console.log(data);
         navigate(`/club/${clubId}/events`);
     }
 
@@ -34,7 +33,7 @@ function AddEvent() {
                 <input className="p-2 m-2 border-yellow-300 border-2" type="text" name="title" placeholder="Enter event title" onChange={(e) => setTitle(e.target.value)} value={title}></input>
                 <textarea className="p-2 m-2 border-yellow-300 border-2" name="description" placeholder="Enter event description" rows={10} onChange={(e) => setDescription(e.target.value)} value={description}></textarea>
                 <input type="file" className="p-2 m-2"></input>
-                <button type="submit" className="py-2 px-4 bg-yellow-300 w-fit rounded-lg m-4 font-bold text-xl">Add</button>
+                <button type="submit" className="py-2 px-4 bg-yellow-300 w-fit rounded-lg m-4 font-bold text-xl">Post</button>
             </form>
         </div>
     );
